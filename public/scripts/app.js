1e85059ec8152457ee8609680ffc54e4226305a1
Thais Contreras <thaisc98@hotmail.es>
2019-10-16 23:19:39 -0400

    Arrow Part 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,43 @@
 'use strict';
 
-//  function square(x){
-//     return x * x ;
-// }
+var add = function add(a, b) {
+    //   console.log(arguments);
+    return a + b;
+};
+console.log(add(55, 1));
+var user = {
+    name: 'Andrew',
+    cities: ['Pholadelphia', 'New York', 'Dublin'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
 
-// console.log(square(3));
+        // const that = this;
+        this.cities.map(function (city) {
+            return _this.name + 'has lived in' + city;
+        });
 
+        // this.cities.forEach((city) =>{
+        //     console.log(this.name + ' has lived in ' + city);
+        // });
+    }
+};
+console.log(user.printPlacesLived());
 
-// // const squareArrow = (x) => {
-// //     return x * x;
-// // }
+var multiplier = {
+    numbers: [1, 2, 3],
+    multiplyBy: 5,
 
+    multiply: function multiply() {
+        var _this2 = this;
 
-// const squareArrow = (x) => x * x;
+        return this.numbers.map(function (x) {
+            return x * _this2.multiplyBy;
+        });
+    }
+    // multiply: function(){
+    //     this.numbers.map((x) => this.multiplyBy * this.x);
+    // }
 
-
-// console.log(squareArrow(4))
-
-
-var fullName = 'Jen Mead';
-
-// let firstName;
-// const getFirstNameL = (x) =>{
-//     return  fullName.split(' ')[0];
-// }
-
-// console.log(getFirstNameL('Luckas Alofnso'))
-
-var getFirstName = function getFirstName(x) {
-  return fullName.split(' ')[0];
 };
 
-console.log(getFirstName('Jean Fuck'));
+console.log(multiplier.multiply());
