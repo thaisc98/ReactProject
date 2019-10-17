38b717b9ed458551e3d2ced758692d11d1f55763
thais.contreras <Thais_Contreras@claro.com.do>
2019-10-17 08:36:53 -0400

    fix

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-var nameVar = 'Andrew';
-
-var nameVar = 'Mike';
-console.log('nameVar',nameVar);
-
-let  nameLet = 'Jen';
-nameLet = 'Julie';
-console.log('nameLet', nameLet);
-
-
-const nameConst = 'franki';
-console.log('nameConst', nameConst);
-
-function getPetName(){
-    var petName = 'Hal';
-    return petName;
-}
-
-getPetName();
-console.log(petName);
-=======
-
 
 //Block scoping
 
@@ -36,4 +13,4 @@ if(fullName){
 }
 
 console.log(firstName);
->>>>>>> 36dd3aa58d69483066491b76238d840c1b98662c
+
