b8a1506e32adcace08f1866018df739ae73c3a09
Thais Contreras <thais_contreras@claro.com.do>
2020-01-09 16:41:10 -0400

    This is importing react lib

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-import isSenior, { isAdult, canDrink } from "./person.js";
-//import "./utils.js";
-//import subtract, { square, add } from "./utils.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("app.js is rssssssssunning");
+const template = React.createElement("p", {}, "testing 1234");
 
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
-
-console.log(isAdult(18));
-console.log(canDrink(21));
-console.log(isSenior(64));
+ReactDOM.render(template, document.getElementById("app"));
